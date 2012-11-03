e4b00afd37e8119378a3d1d66aa7551b9a4991c4
greggay <greggay@debian.myhome.westell.com>
2012-11-02 22:21:12 -0400

    Test for BST...the IOS is working and print out the tree in a tree like formation is working.

--- tree_test.cpp
+++ tree_test.cpp
@@ -6,44 +6,58 @@ using std::endl;
 
 int main() {
   BST<int>* bst = new BST<int>();
-/*
-  bst->insert(5);
-  bst->insert(6);
-  bst->insert(3);
-  bst->insert(2);
-  bst->insert(7);
   
-  bst->print();
-
-  cout << endl;
-  //bst->insert(12);
-  bst->remove(6);
-  
-  bst->print();*/
-
-
+  cout << "Tree Print" << endl;
   bst->insert(10);
   bst->insert(5);
+  bst->insert(7);
+  bst->insert(3);
+  bst->insert(2);
   bst->insert(15);
   bst->insert(16);
-  bst->insert(13);
+  bst->insert(1);
   bst->insert(12);
-  bst->insert(17);
-  bst->print();
-    
-  cout<< endl;
+  bst->treePrint();
 
-  bst->remove(15);
-  bst->print();
-  
-  cout<< endl;
+  cout << endl;
   
   bst->remove(10);
-  bst->print();
+  bst->treePrint();
   
   cout << endl;
-/*
-  bst->remove(13);
-  bst->print();
-*/
+
+  bst->remove(12);
+  bst->treePrint();
+  
+  cout << endl;
+
+  bst->remove(5);
+  bst->treePrint();
+  
+  cout << endl;
+
+  bst->remove(1);
+  bst->treePrint();
+
+  cout << endl;
+
+  bst->remove(3);
+  bst->treePrint();
+
+  cout << endl;
+
+  bst->remove(16);
+  bst->treePrint();
+
+  cout << endl;
+
+  bst->remove(15);
+  bst->treePrint();
+
+  cout << endl;
+
+  bst->remove(2);
+  bst->treePrint();
+
+  cout << "End of Remove" << endl;
 }
