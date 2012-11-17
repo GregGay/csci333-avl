ff711e30b157ca3284a037c94759d9f9c03d48ba
greggay <greggay@debian.myhome.westell.com>
2012-11-16 23:48:06 -0500

    Rotations are working

--- src/avl_test.cpp
+++ src/avl_test.cpp
@@ -6,14 +6,16 @@ using std::endl;
 
 int main() {
   AVL<int>* avl = new AVL<int>();
+  //AVL<double>* avlD = new AVL<double>();
+  //AVL<std::string>* avlS = new AVL<std::string>();
   
   cout << "Pre Order Traversal Print" << endl;
   cout << endl;
-  avl->insert(1);
-  avl->insert(2);
   avl->insert(4);
+  avl->insert(2);
+  avl->insert(3);
   avl->insert(7);
-  avl->insert(8);
+  avl->insert(1);
   avl->print();
   /*cout << endl;
   cout << "Post Order Traversal" << endl;
